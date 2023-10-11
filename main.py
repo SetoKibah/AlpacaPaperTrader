d26cdaad5de0e0945278373a56b183cefff7ef34
SetoKibah <setokibah@gmail.com>
2023-10-10 22:27:50 -0600

    Made to work with Alpaca, generates orders as expected.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from alpaca.data.historical import StockHistoricalDataClient
 from alpaca.data.requests import StockBarsRequest
 from alpaca.trading.client import TradingClient
 from alpaca.data.timeframe import TimeFrame, TimeFrameUnit
+from alpaca.trading.requests import OrderRequest
 from datetime import datetime
 from time import sleep
 
@@ -21,36 +22,63 @@ for symbol in symbols:
     bar_request = StockBarsRequest(
         symbol_or_symbols=symbol, 
         timeframe=TimeFrame(1, TimeFrameUnit('Day')), 
-        start=datetime(2023, 1, 1), 
-        end=datetime(2023, 9, 30), 
+        start=datetime(2022, 11, 1), 
+        end=datetime(2023, 10, 9), 
         limit=200)
     
     bars = client.get_stock_bars(bar_request)
+    """
     print(f"\n\n")
     print(bars[symbol][0])
     print(f"\n\n\n")
-    exit()
+    """
 
     short_window = 50
     long_window = 200
+    #print(f'Confirming data point length({len(bars[symbol])}) vs long window({long_window})')
+    if len(bars[symbol]) >= long_window:  # Check to ensure we have enough data points
+        print('Data points confirmed...')
+        short_moving_average = sum([bar.open for bar in bars[symbol][-short_window:]]) / short_window
+        long_moving_average = sum([bar.open for bar in bars[symbol][-long_window:]]) / long_window
 
-    if len(bars) >= long_window:  # Check to ensure we have enough data points
-        short_moving_average = sum([bar.open for bar in bars[-short_window:]]) / short_window
-        long_moving_average = sum([bar.open for bar in bars[-long_window:]]) / long_window
+        print(f'Short Average: {short_moving_average}\nLong Average: {long_moving_average}')
 
         try:
+            print(f'Getting {symbol} position...')
             position = trading_client.get_position(symbol)
+            print(f'Position: {position}')
         except:
+            print(f'No position for {symbol}.')
             position = None
 
         if short_moving_average > long_moving_average and not position:
             # Golden Cross - Buy Signal
-            trading_client.submit_order(symbol=symbol, qty=10, side='buy', type='market', time_in_force='gtc')
+
+            # Generate order request object
+            order = OrderRequest(
+                symbol=symbol,
+                qty=10,
+                side='buy',
+                type='market',
+                time_in_force='gtc'
+            )
+
+            trading_client.submit_order(order)
             print(f"Golden Cross detected for {symbol}. Buying 10 shares.")
 
         elif short_moving_average < long_moving_average and position:
             # Death Cross - Sell Signal
-            trading_client.submit_order(symbol=symbol, qty=position.qty, side='sell', type='market', time_in_force='gtc')
+
+            # Generate order request object
+            order = OrderRequest(
+                symbol=symbol,
+                qty=position.qty,
+                side='sell',
+                type='market',
+                time_in_force='gtc'
+            )
+            
+            trading_client.submit_order(order)
             print(f"Death Cross detected for {symbol}. Selling {position.qty} shares.")
 
 account = trading_client.get_account()
